fbecd428e1cbbd0291a9f3ec2a6bda695e410972
roroxs16 <38821332+roroxs16@users.noreply.github.com>
2021-01-12 22:39:29 -0300

    asds

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,20 +1,23 @@
 /* Populate tabla categoria*/
-INSERT INTO categoria(id, nombre) VALUES(1, 'Mesas');
-INSERT INTO categoria(id, nombre) VALUES(2, 'Sillas');
-INSERT INTO categoria(id, nombre) VALUES(3, 'Comedores');
-INSERT INTO categoria(id, nombre) VALUES(4, 'Oficina');
-INSERT INTO categoria(id, nombre) VALUES(5, 'Exteriores');
-INSERT INTO categoria(id, nombre) VALUES(6, 'Interiores');
+INSERT INTO categoria(id, nombre) VALUES(1, 'Closet');
+INSERT INTO categoria(id, nombre) VALUES(2, 'Cocina');
+INSERT INTO categoria(id, nombre) VALUES(3, 'Mesas');
+INSERT INTO categoria(id, nombre) VALUES(4, 'Escritorio');
+INSERT INTO categoria(id, nombre) VALUES(5, 'Repisas');
 
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(1, 'Mesa de madera nacional fabricada y tallada artesanalmente de color crema', 'Mesa redonda', '687', '466666','Habilitado', '1');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(2, 'Mesa de roble nacional de color claro', 'Mesa de roble ancestral', '687', '100000','Habilitado', '1');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(3, 'Mesa de alerce europeo de color marrón claro', 'Mesa cuadrada de alerce', '687', '150000','Habilitado', '1');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(4, 'Silla de cerezo japones exportada desde japón', 'Silla de cerezo japones', '687', '40000','Habilitado', '2');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(5, 'Comedor familiar de color burdeo para 8 personas', 'Comedor familiar', '687', '466666','Habilitado', '3');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(6, 'Lampara de madera inspirada en la naturaleza', 'Lampara de techo', '687', '100000','Habilitado', '6');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(7, 'Candelabro de techo tallado en madera nacional', 'Lampara Colgante', '687', '100000','Deshabilitado', '6');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(8, 'Escritorio de madera en forma de L recubierto con melamina de color marrón', 'Escritorio de oficina en forma de L', '687', '150000','Habilitado', '4');
-INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(9, 'Escritorio de madera en forma de L recubierto con melamina de color gris', 'Escritorio de oficina en forma de L', '687', '40000','Habilitado', '4');
+
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(1, 'Estructura en madera de 22 mm, frentes y puertas en madera barnizada. Dimensiones 2.00mx0.60mx2.00m', 'CLOSET RAULI ', '55', '450000','Habilitado', '1');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(2, 'Estructura en melamina blanca , frentes de cajón madera lacada. Dimensiones 2.00mx0.60mx2.00m', 'CLOSET BLANCO ', '23', '450000','Habilitado', '1');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(3, 'Estructura en madera de pino barnizada. Dimensiones 4.00m x 0.50mx2.00m', 'CLOSET DESPENSA', '10', '400000','Habilitado', '1');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(4, 'Estructura cubierta por melamina de color granito en 15mm, puertas de color granito de 18mm', 'COCINA MELAMINA COLOR GRANITO', '5', '450000','Habilitado', '2');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(5, 'Estructura en melamina blanca, puertas cubiertas con melamina blanca 15 MM, cubierta posformada', 'COCINA BLANCA', '5', '350000','Habilitado', '2');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(6, 'Mesa de madera de raulí. Dimensiones 150cmX50cmx75cm', 'MESA ARRIMO DE MADERA','15', '230000','Habilitado', '3');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(7, 'Mesa de madera de rauli. Dimensiones 60cmX50cmX60cm', 'MESA DE CENTRO', '50', '190000','Habilitado', '3');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(8, 'Escritorio hecho en madera y cubierto con melamina de color blanca. Dimensiones 150cmX60cmX75cm', 'ESCRITORIO BLANCO', '20', '200000','Habilitado', '4');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(9, 'Escritorio de madera en forma de L recubierto con melamina de color gris', 'ESCRITORIO EN FORMA DE L', '5', '30000','Habilitado', '4');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(10, 'Repisa lacada. Dimensiones 80cmX30cmx150cm', 'REPISA LACADA', '10', '200000','Habilitado', '5');
+INSERT INTO productos(id, descripcion,nombre,stock,valor_unitario,estado,categoria_id) VALUES(11, 'Repisa fabricada con madera de rauli. Dimensiones 40cmX20cmX30cm', 'REPISA DE MADERA DE RAULI', '10', '90000','Habilitado', '5');
+
 
 INSERT INTO usuarios(id, apellido,ciudad,direccion,email,nombre,password,rut,numero_telefono) VALUES(1, 'Cifuentes', 'Chillán', 'Monte Tibidabo 1339', 'rodrigo.cifuentes@gmail.com','Rodrigo', '$2a$10$UZDG2bZjYlnhHh9Yrpqkketyq2lj92JprtHqDLCZv0/LegbuWlVC.','19.090.005-3', 942750783);
 INSERT INTO usuarios(id, apellido,ciudad,direccion,email,nombre,password,rut,numero_telefono) VALUES(2, 'Martinez', 'Chillán', 'Monte Tibidabo 1339', 'Carolina.Martinez@gmail.com','Carolina', '$2a$10$wTlcvl7x5YRKPb410r1plOlx6/Yb5F3iw9gejGaLCp8/tCusj91Me','14.292.654-7', 942750783);
